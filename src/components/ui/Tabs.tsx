611bf95c2d936cbac4897186a5959c7ec01e8e32
MV <mv@scrinfo.net>
2025-08-26 14:00:03 +0200

    update tabs

--- src/components/ui/Tabs.tsx
+++ src/components/ui/Tabs.tsx
@@ -1,50 +1,31 @@
-import { useContext, useState } from "react";
+import { ComponentType, useContext, useState } from "react";
 import { Animated, ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, useWindowDimensions, View, ViewProps } from "react-native";
 import { SceneMap, TabView } from "react-native-tab-view";
 import { ScrUiContext } from "../../contexts/ScrUiContext";
 import { Color } from "../../types/Color";
 import { Button } from "./Button";
 
-const FirstRoute = () => (
-  <View style={{ flex: 1, backgroundColor: '#ff4081' }} />
-);
-
-const SecondRoute = () => (
-  <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
-);
-
-const renderScene = SceneMap({
-  first: FirstRoute,
-  second: SecondRoute,
-  third: FirstRoute,
-  fourth: SecondRoute,
-  fifth: FirstRoute,
-  sixth: SecondRoute,
-});
-
-const routes = [
-  { key: 'first', title: 'First' },
-  { key: 'second', title: 'Second' },
-  // { key: 'third', title: 'Third' },
-  // { key: 'fourth', title: 'Fourth' },
-  // { key: 'fifth', title: 'Fifth' },
-  // { key: 'sixth', title: 'Sixth' },
-];
-
 type TabsProps = ViewProps & {
+  routes: { value: string; title: string, route: ComponentType<any> }[];
   scrollable?: boolean;
 }
 
 export const Tabs = ({
+  routes,
   scrollable = false,
 }: TabsProps) => {
   const layout = useWindowDimensions();
   const [index, setIndex] = useState(0);
   const {colors} = useContext(ScrUiContext);
+
+  const renderScene = SceneMap(routes.reduce((acc, route) => {
+    acc[route.value] = route.route;
+    return acc;
+  }, {} as Record<string, ComponentType<any>>));
   
   return (
     <TabView
-      navigationState={{ index, routes }}
+      navigationState={{ index, routes: routes.map(route => ({ key: route.value, title: route.title })) }}
       renderScene={renderScene}
       onIndexChange={setIndex}
       initialLayout={{ width: layout.width }}
@@ -76,10 +57,10 @@ export const Tabs = ({
               {routes.map((route, i) => {
                 return (
                   <TouchableOpacity
-                    key={route.key}
-                    style={[styles(colors).tabItem, {backgroundColor: route.key === routes[index].key ? colors.background : colors.muted}]}
-                    onPress={() => props.jumpTo(route.key)} activeOpacity={0.8}>
-                    <Animated.Text style={{ opacity, color: route.key === routes[index].key ? colors.text : colors.mutedText }}>{route.title}</Animated.Text>
+                    key={route.value}
+                    style={[styles(colors).tabItem, {backgroundColor: route.value === routes[index].value ? colors.background : colors.muted}]}
+                    onPress={() => props.jumpTo(route.value)} activeOpacity={0.8}>
+                    <Animated.Text style={{ opacity, color: route.value === routes[index].value ? colors.text : colors.mutedText }}>{route.title}</Animated.Text>
                   </TouchableOpacity>
                 )
               })}
